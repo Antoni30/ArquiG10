ecf7080c296639a6687cf2af7b9440ba1be53b51
Rich4rd16 <rich4rd16@gmail.com>
2025-10-21 14:09:38 -0500

    feat: Agregar carpeta web para SOAP sin BDD

---
+++ TI1.1 SOAP-JAVA-SIN_BD_GR10/02. CLIWEB/soap-web/src/components/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+    const [usuario, setUsuario] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Credenciales Fijas del proyecto
+        const USUARIO_CORRECTO = "MONSTER";
+        const CLAVE_CORRECTA = "MONSTER9";
+
+        if (usuario === USUARIO_CORRECTO && password === CLAVE_CORRECTA) {
+            setError('');
+            onLogin(true); // Login exitoso
+        } else {
+            setError('Credenciales incorrectas. Intente de nuevo.');
+        }
+    };
+
+    return (
+        <div className="login-main-container"> 
+            {/* Sección del formulario (izquierda) */}
+            <div className="login-form-section"> 
+                <h2>Inicio de Sesión</h2>
+                {error && <p className="message result-error">{error}</p>}
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>Usuario:</label>
+                        <input type="text" value={usuario} onChange={(e) => setUsuario(e.target.value)} required />
+                    </div>
+                    <div>
+                        <label>Contraseña:</label>
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    </div>
+                    <button type="submit">Iniciar Sesión</button>
+                </form>
+            </div>
+
+            {/* Sección de la imagen (derecha) */}
+            <div className="login-image-section">
+                <img src="/Monster.jpg" alt="Escudo de Liga Deportiva Universitaria de Quito" />
+            </div>
+        </div>
+    );
+};
+
+export default Login;
